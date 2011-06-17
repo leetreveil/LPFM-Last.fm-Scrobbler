ffbed85ea493ea660834831979dc6757983ad1b1
Daniel@BURGER3 <Daniel@BURGER3>
2011-06-17 21:36:37 +1200

    DLL Release v1.0

--- LastFmScrobbler/Properties/AssemblyInfo.cs
+++ LastFmScrobbler/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Lpfm.LastFmScrobbler")]
-[assembly: AssemblyDescription("A Last.FM Scrobbler library")]
+[assembly: AssemblyDescription("A Last.fm Scrobbler library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LPFM")]
 [assembly: AssemblyProduct("Lpfm.LastFmScrobbler")]
